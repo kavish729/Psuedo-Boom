11679d2afe821156627c291434195a6ce7c04ea5
Prayash <prayashchhetri221@gmail.com>
2022-07-28 00:06:49 +0530

    inital prayash

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Kavish branch </h1>
-      <h1>Prayash branch</h1>
-      <h1>kavish check</h1>{" "}
-    </div>
+   <div>
+    
+   </div>
   );
 }
 
